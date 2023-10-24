8095346098b4de0a0376ca5a3950b69cefeb78d4
Alexander Giffah <giffahalexander@gmail.com>
2023-10-24 13:21:43 +0000

    Added a new lesson code

--- 06concatenation.py
+++ 06concatenation.py
@@ -1,5 +1,13 @@
 # Python concatenation
 # This simply means joining two or more string values together
+firstName ='Alexander'
+lastName ='Giffah'
 
+
+# fullName=firstName+ ' ' +lastName
+# print(fullName)
 
 # Formatted String
+
+fullName = f'{firstName} {lastName}'
+print(fullName)
