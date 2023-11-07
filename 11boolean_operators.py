ff188f7b1f3be372fdef22f97e131520fabce062
Alexander Giffah <giffahalexander@gmail.com>
2023-11-07 13:15:45 +0000

    Added new code

--- 11boolean_operators.py
+++ 11boolean_operators.py
@@ -7,3 +7,16 @@
 # or
 
 # not
+
+
+Valid_age = 18
+Valid_country ='Ghana'
+
+Age = int(input("Enter your age: "))
+country = input("Enter your country: ")
+
+if Age == Valid_age and country == Valid_country:
+    print("You are eligible to vote in  Ghana")
+else:
+    print("You are not eligible to vote in Ghana")
+
