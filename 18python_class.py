99a6e1417a65c7d1e6938be4c8bc493645e03068
Alexander Giffah <giffahalexander@gmail.com>
2023-12-19 12:43:51 +0000

    New updated

--- 18python_class.py
+++ 18python_class.py
@@ -1,6 +1,13 @@
 # A class is a blue-print for creating objects.
+class BakingPan:
+    def __init__(self,flour,sugar,special_ingredient):
+        self.hOUR= flour 
+        self.sugar= sugar
+        self.special_ingredient= special_ingredient
 
 
+Bread = BakingPan('soft',20,'cocoa')
+print(Bread.hOUR)
 
 
 
